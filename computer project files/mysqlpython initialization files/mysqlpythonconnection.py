e481f10653c67028eecb87a6c37783127ba520ec
sibi15 <99868966+sibi15@users.noreply.github.com>
2023-05-03 15:35:59 +0530

    Delete computer project files/mysqlpython initialization files directory

--- computer project files/mysqlpython initialization files/mysqlpythonconnection.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-con=mysql.connector.connect(host="localhost", user="root", password="kobe_24@lakers_8_2_messi_10")
-if con.is_connected():
-    print("Connection Successful")
-#con is the connection object
-cur=con.cursor()                    #CursorObject=ConnectionObject.cursor()
-cur.execute("create database tests") #CursorObject.execute(<SQL Query string>)
-print("Query executed successfully")
